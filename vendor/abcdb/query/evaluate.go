446690e6778a127ffa874ec8c61f7a0476f8ea46
VinaLx <vinaleux@gmail.com>
2019-02-10 21:17:39 +0800

    complete evaluate interface

--- vendor/abcdb/query/evaluate.go
+++ vendor/abcdb/query/evaluate.go
@@ -2,13 +2,31 @@ package query
 
 import (
 	"abcdb/sql/parser/ast"
+	"abcdb/storage"
+	"io"
 )
 
-// Evaluator of query `Plan`
+// Evaluator is
 type Evaluator struct {
+	Storage *storage.DataStore
+	// TODO
 }
 
-// Execute the sql statement
-func Execute(sql ast.SQL) {
+// Execute the SQL statement
+//
+// A `Select` query (or subquery) is first transformed to relational algebra
+//   (see 'abcdb/query/algebra/'), and then corresponding query plan is made,
+//   which is finally evaluated with the `Evaluator`
+//
+// There're other queries other than `Select`, which are mostly relatively
+//   straightforward to implement with the help of the interfaces of Storages.
+//
+// During the execution,
+//   appropriate text output is generated and write to the `io.Writer`.
+//   And errors yielded by low level interfaces are appropriately handled.
+func Execute(
+	ds *storage.DataStore, ts *storage.TableStore,
+	sql ast.SQL, output io.Writer) error {
 	// TODO
+	return nil
 }
