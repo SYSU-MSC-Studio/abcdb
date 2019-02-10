4b33076431fdbbecb20545582b6d9f01bb7823b5
yanbin <yanb25@mail2.sysu.edu.cn>
2019-02-11 00:23:23 +0800

    change func transform to interface Transformer

--- vendor/abcdb/query/algebra/transform.go
+++ vendor/abcdb/query/algebra/transform.go
@@ -11,14 +11,24 @@ import (
 // - input sql is guaranteed (or assumed) to be perfectly valid
 // - output operator should express identical semantic of input SQL
 func FromAST(sql ast.Select) Operator {
-	// TODO
-	return transform(nil)
+	// TODO:
+	return BasicTransformer{}.Transform(nil)
 }
 
-// basic relational algebra equivalent transformation.
+// Transformer performs algebra equivalent transformations
+// to a relational algebra operator tree (see query/algebra/operators.go for operator tree).
+type Transformer interface {
+	Transform(Operator) Operator
+}
+
+// BasicTransformer performs very basic (or even trivial) transformations
+// to relational algebra operator trees.
+//
 // see [wiki](https://en.wikipedia.org/wiki/Relational_algebra#Use_of_algebraic_properties_for_query_optimization)
-// for more information
-func transform(op Operator) Operator {
-	// TODO
-	return op
+// for more information.
+type BasicTransformer struct{}
+
+func (t BasicTransformer) Transform(root Operator) Operator {
+	// TODO:
+	return root
 }
